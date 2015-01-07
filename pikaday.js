4c0bdec353976a9fb9cad37fa5e3e5e7a9f4b893
Steffen Gransow <graste@mivesto.de>
2015-01-07 15:29:29 +0100

    add touchstart event to prevent fastclick problems

--- pikaday.js
+++ pikaday.js
@@ -604,6 +604,7 @@
         self.el.className = 'pika-single' + (opts.isRTL ? ' is-rtl' : '');
 
         addEvent(self.el, 'mousedown', self._onMouseDown, true);
+        addEvent(self.el, 'touchstart', self._onMouseDown, true);
         addEvent(self.el, 'change', self._onChange);
 
         if (opts.field) {
@@ -1220,6 +1221,7 @@
         {
             this.hide();
             removeEvent(this.el, 'mousedown', this._onMouseDown, true);
+            removeEvent(this.el, 'touchstart', this._onMouseDown, true);
             removeEvent(this.el, 'change', this._onChange);
             if (this._o.field) {
                 removeEvent(this._o.field, 'change', this._onInputChange);
